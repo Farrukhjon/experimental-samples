f4fe4567ed6dd1def01ee304374017ec646d8938
Farrukhjon Sattorov <farrukhjon.sattorov@gmail.com>
2016-08-22 13:31:52 +0500

    added simple jms experiment

---
+++ spring/spring-jms-experiment/src/test/java/org/farrukh/example/spring/jms/ApplicationTests.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) F.D. Sattorov Systems, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by F.D. Sattorov <farrukhjon.sattorov@gmail.com>, May 2016.
+ */
+
+package org.farrukh.example.spring.jms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
