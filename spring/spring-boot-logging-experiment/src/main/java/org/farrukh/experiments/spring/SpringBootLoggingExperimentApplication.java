37dc3bfcd3562595dd39612c75cd92b1c3c8ecdf
Farrukhjon D. Sattorov <farrukhjon.sattorov@gmail.com>
2016-11-13 11:39:59 +0300

    added experimental project for the spring boot and logback

---
+++ spring/spring-boot-logging-experiment/src/main/java/org/farrukh/experiments/spring/SpringBootLoggingExperimentApplication.java
@@ -0,0 +1,12 @@
+package org.farrukh.experiments.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLoggingExperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLoggingExperimentApplication.class, args);
+	}
+}
