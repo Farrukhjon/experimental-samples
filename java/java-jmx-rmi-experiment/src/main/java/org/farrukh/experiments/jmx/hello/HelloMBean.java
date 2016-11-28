bef0de9d0f48431df8ba2212ed9e404824656da8
Farrukhjon D. Sattorov <farrukhjon.sattorov@excelian.com>
2016-11-28 15:33:57 +0300

    added the jmx experiment

---
+++ java/java-jmx-rmi-experiment/src/main/java/org/farrukh/experiments/jmx/hello/HelloMBean.java
@@ -0,0 +1,7 @@
+package org.farrukh.experiments.jmx.hello;
+
+public interface HelloMBean {
+    public void setMessage(String message);
+    public String getMessage();
+    public void sayHello();
+ }
