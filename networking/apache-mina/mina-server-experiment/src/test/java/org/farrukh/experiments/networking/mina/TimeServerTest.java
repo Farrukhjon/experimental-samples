6f186733a49794bdc05e106b40f60034a4848aa6
Farrukhjon D. Sattorov <farrukhjon.sattorov@excelian.com>
2016-12-05 18:05:03 +0300

    added networking experiment using mina framework

---
+++ networking/apache-mina/mina-server-experiment/src/test/java/org/farrukh/experiments/networking/mina/TimeServerTest.java
@@ -0,0 +1,17 @@
+package org.farrukh.experiments.networking.mina;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class TimeServerTest {
+
+    @Test
+    public void shouldStartTimeServer() throws IOException {
+        TimeServer.main(new String[]{});
+        
+    }
+}
