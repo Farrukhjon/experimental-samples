2dc31c1b517b1ab287a456bf495042336f9fd1bc
farrukhjon <farrukhjon.sattorov@gmail.com>
2017-06-07 15:43:50 +0300

    update

--- java/java-collection-generic-experiment/java6-collections-experiments/src/main/java/org/farrukh/experiments/SubListExperiment.java
+++ java/java-collection-generic-experiment/java6-collections-experiments/src/main/java/org/farrukh/experiments/SubListExperiment.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class SubListExperiment {
 
-    public static final int COUNT_OF_GET = 25;
+    private static final int COUNT = 25;
 
     public static void main(String[] args) {
         List<Integer> originalList = new ArrayList<Integer>();
@@ -14,34 +14,12 @@ public class SubListExperiment {
             originalList.add(i);
         }
 
-/*        for (int i = 0; i < originalList.size(); i++) {
-            if (i > 0 && isDivisibleBy(i)) {
-                List<Integer> subList = originalList.subList(0, COUNT_OF_GET);
-                sendRequest(subList);
-                originalList.subList(0, COUNT_OF_GET).clear();
-                System.out.println("Clearing up");
-            }
-            if (originalList.size() <= COUNT_OF_GET ) {
-                sendRequest(originalList);
-            }
-        }
-        */
-        while(originalList.size() >= COUNT_OF_GET) {
-            List<Integer> subList = originalList.subList(0, COUNT_OF_GET);
-            sendRequest(subList);
-            originalList.subList(0, COUNT_OF_GET).clear();
-            System.out.println("Clearing up");
+        while(originalList.size() >= COUNT) {
+            List<Integer> subList = originalList.subList(0, COUNT);
+            System.out.println(String.format("Sub list of %s is %s", originalList, subList));
+            originalList.subList(0, COUNT).clear();
         }
         System.out.println("sent:" + originalList);
-        sendRequest(originalList);
-    }
-
-    private static void sendRequest(List<Integer> list) {
-        System.out.println(String.format("Handling the request for items: %s", list));
-    }
-
-    private static boolean isDivisibleBy(int aNumber) {
-        return aNumber % COUNT_OF_GET == 0;
     }
 
 }
