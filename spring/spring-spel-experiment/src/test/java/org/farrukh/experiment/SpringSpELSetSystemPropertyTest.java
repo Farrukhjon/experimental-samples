7ae6a091bc6b14505ca8c1a58ef05954b151620d
Farrukhjon D. Sattorov <farrukhjon.sattorov@gmail.com>
2018-05-21 23:33:42 +0300

    update

--- spring/spring-spel-experiment/src/test/java/org/farrukh/experiment/SpringSpELSetSystemPropertyTest.java
+++ spring/spring-spel-experiment/src/test/java/org/farrukh/experiment/SpringSpELSetSystemPropertyTest.java
@@ -19,15 +19,27 @@ public class SpringSpELSetSystemPropertyTest {
     @Autowired
     Book book;
 
- /*   @BeforeClass
+    @Autowired
+    Book book2;
+
+    @BeforeClass
     public static void setUp() throws Exception {
         String cmaSeparatedStrAuthors = "Ali, Vali, Surayo, Muhayo";
         System.setProperty("book.authors", cmaSeparatedStrAuthors);
-    }*/
+
+        System.setProperty("book2.authors", "Sami, Gani");
+    }
 
     @Test
-    public void test() {
+    public void testBook() {
         List<String> authors = book.getAuthors();
         assertEquals(4, authors.size());
     }
+
+    @Test
+    public void testBook2() {
+        List<String> authors = book2.getAuthors();
+        assertEquals(2, authors.size());
+    }
+
 }
