aa9d83467563fae393e73fc41ad38285f7c0cbe9
farrukhjon <farrukhjon.sattorov@gmail.com>
2018-05-25 15:31:01 +0300

    init commit

--- spring/spring-spel-experiment/src/test/java/org/farrukh/experiment/SpringSpELSetSystemPropertyTest.java
+++ spring/spring-spel-experiment/src/test/java/org/farrukh/experiment/SpringSpELSetSystemPropertyTest.java
@@ -4,11 +4,13 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -19,15 +21,29 @@ public class SpringSpELSetSystemPropertyTest {
     @Autowired
     Book book;
 
- /*   @BeforeClass
+    @Value("#{systemProperties[abc].split(',')}")
+    private List<String> aListFromArrayProvidedBySystemproperties;
+
+    @BeforeClass
     public static void setUp() throws Exception {
         String cmaSeparatedStrAuthors = "Ali, Vali, Surayo, Muhayo";
         System.setProperty("book.authors", cmaSeparatedStrAuthors);
-    }*/
+        System.setProperty("abc", "A,B,C");
+
+    }
 
     @Test
     public void test() {
         List<String> authors = book.getAuthors();
         assertEquals(4, authors.size());
+    }
+
+    @Test
+    public void test1() {
+        List<String> aListFromArrayProvidedBySystemproperties = this.aListFromArrayProvidedBySystemproperties;
+        String[] expected = {"A", "B", "C"};
+        String[] actual = aListFromArrayProvidedBySystemproperties.toArray(new String[]{});
+
+        assertArrayEquals(expected, actual);
     }
 }
