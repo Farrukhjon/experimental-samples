e65d8d64a2da1299478d0e8fb9d91fd714109b21
farrukhjon <farrukhjon.sattorov@gmail.com>
2019-01-11 16:16:12 +0300

    update

--- java/java-exception-experiment/src/main/org/farrukh/examples/exceptions/Inheritance.java
+++ java/java-exception-experiment/src/main/org/farrukh/examples/exceptions/Inheritance.java
@@ -1,4 +1,25 @@
 package org.farrukh.examples.exceptions;
 
 public class Inheritance {
+
+    public static void main(String[] args) {
+        try {
+            throwsParentException();
+        } catch (Exception e) {
+            System.out.println(e.getCause());
+            System.out.println(e.getClass());
+            System.out.println(e);
+        }
+    }
+
+    static class ParentException extends Exception {
+        public ParentException(final String message) {
+            super(message);
+        }
+    }
+
+    private static void throwsParentException() throws Exception {
+        throw new RuntimeException(new ParentException("Parent exception is thrown"));
+    }
+
 }
